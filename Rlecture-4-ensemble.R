21ae5a40fbc2dc249de612c2de1ccca6667bbf91
T. KANAMORI <35907655+kanamori-takafumi@users.noreply.github.com>
2019-07-16 19:07:31 +0900

    Add files via upload

--- Rlecture-4-ensemble.R
+++ Rlecture-4-ensemble.R
@@ -34,6 +34,9 @@ rpf$control$cp # regularization par.
 cat("rpart: training error =",mean(predict(rpf,type='class') != d$y)) 
 par(mfrow=c(1,1)); rpart.plot(rpf, roundint=FALSE) # plot
 
+# exercise
+# rpf <- rpart(y ~ x.grade + x.g2, d, control=rpart.control(cp=0.1)) # rpart
+# par(mfrow=c(1,1)); rpart.plot(rpf, roundint=FALSE) # plot
 
 
 ##############################
@@ -71,44 +74,38 @@ plot(td[,1:2],col=rp_pred,main='rpart'); plot(td[,1:2],col=ba_pred,main='bagging
 ##############################
 ## R seminar 4-3: random forest
 ############################## 
+# libraries
 require(randomForest) # randomForest
-require(rpart) # rpart
-require(ipred) # bagging
+require(rpart)        # stagec data
 
-# spiral data
-require(mlbench) # mlbench.spirals
- d <- data.frame(mlbench.spirals(200, 1.3,sd=0.1))  # training data
-td <- data.frame(mlbench.spirals(1000,1.3,sd=0.1))  # test data
-par(mfrow=c(1,1)); plot(d[,1:2],col=d$c,lwd=2,main="data")
+# stagec data
+d <- data.frame(x=stagec[3:8],y=as.factor(stagec$pgstat))
+dim(d)
 
-# random forest with 100 trees
-rf <- randomForest(classes~.,d,ntree=100); rf_pred <- predict(rf,td,type='class') # prediction
-cat("random forest: test error =",mean(rf_pred != td$c))  # test error
+# random forest
+rf <- randomForest(y~., d, ntree=1000, importance=TRUE, na.action=na.omit)
+# importance of variables
+importance(rf)[,3:4]
 
-# other methods: rpart
-rp <- rpart(classes~., d); rp_pred <- predict(rp,td,type='class') # prediction
-cat("rpart: test error =",mean(rp_pred != td$c))  # test error
+varImpPlot(rf)  # plot
 
-# other methods: bagging
-ba <- bagging(classes~., d, mfinal=100); ba_pred <- predict(ba,td,type='class') # prediction
-cat("bagging: test error =",mean(ba_pred != td$c)) # test error
+# iris data
+data(iris)
+d <- data.frame(x=iris[,-5], y=as.factor(iris[,5]))
+dim(d)
 
-# plot: rpart vs. RandomForest
-par(mfrow=c(1,2)); 
-plot(td[,1:2],col=rp_pred,main='rpart'); plot(td[,1:2],col=rf_pred,main='RandomForest')
+# random forest
+rf <- randomForest(y~., d, ntree=1000,importance=TRUE, na.action=na.omit)
+# importance of variables
+importance(rf)[,4:5]
 
-# plot: bagging vs. RandomForest
-par(mfrow=c(1,2))
-plot(td[,1:2],col=ba_pred,main='bagging');plot(td[,1:2],col=rf_pred,main='RandomForest')
-
+varImpPlot(rf) # plot
 
 
 ##############################
 ## R seminar 4-4: boosting
 ############################## 
 # library
-require(rpart)        # rpart
-require(ipred)        # bagging
 require(randomForest) # randomForest
 require(xgboost)      # xgboost
 require(kernlab)      # spam data
@@ -124,7 +121,7 @@ te <- list(x=as.matrix(x[-idx,]),y=y[-idx]) # test data
 d <- data.frame(tr$x, y=as.factor(tr$y))  # data frame
 
 # ensemble learning: setting
-T <- 2000     # num. of trees
+T <- 1000     # num. of trees
 
 # XGBoost
 xgb  <- xgboost(data=tr$x, label=tr$y, 
@@ -135,22 +132,14 @@ xgb  <- xgboost(data=tr$x, label=tr$y,
 xgb_pred <- predict(xgb, te$x) > 1/2   # predict
 cat("xgboost: test error =",mean(xgb_pred != te$y))     # test error
 
-# rpart
-rp <- rpart(y~., d);rp_pred <- predict(rp,data.frame(te$x), type='class'); 
-cat("rpart: test eror =",mean(rp_pred != te$y))  # predict & test error
-
-# bagging
-ba <- bagging(y~., d, mfinal=T); ba_pred <- predict(ba,te$x,type='class') # prediction
-cat("bagging: test error =",mean(ba_pred != te$y)) # test error
-
 # random forest
-rf <- randomForest(y~., d, ntree=T); rf_pred <- predict(rf,te$x,type='class') # predict
+rf <- randomForest(y~., d, ntree=T)
+rf_pred <- predict(rf,te$x,type='class') # predict
 cat("random forest: test error =",mean(rf_pred != te$y)) # test error 
 
 # cross validation for xgboost
-K <- 5
 xcv <- xgb.cv(data=tr$x, label=tr$y, 
-              nfold=K,    # K-cross validation
+              nfold=5,    # 5-cross validation
               nround=T,   # T rounds
               metrics="error", 
               objective="binary:logistic",
