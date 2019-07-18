0f8ebd63a916e35541abb4e8f23fbe32e586d700
T. KANAMORI <35907655+kanamori-takafumi@users.noreply.github.com>
2019-07-18 10:39:23 +0900

    Add files via upload

---
+++ Rscripts.r
@@ -0,0 +1,15 @@
+
+PlotDB <- function(sv,dat,len=400,main=NULL){
+  x <- dat$x; cl <- as.numeric(dat$cl)
+  r1 <- range(x[,1]); r2 <- range(x[,2])
+  # plot training data
+  plot(data.frame(x),col=cl,pch=cl,xlim=r1,ylim=r2,lwd=2,cex=1.1,axes=TRUE,main=main)
+  # label prediction on lattice
+  x1 <- seq(r1[1],r1[2],l=len); x2 <- seq(r2[1],r2[2],l=len)
+  X <- expand.grid(x1,x2)                                
+  Y <- as.numeric(predict(sv,X)); Y <- matrix(Y,length(x1))  
+  for(i in 1:max(Y)){  # plot decision boundary
+    contour(x1,x2,Y==i,levels=0.5,lwd=2,lty=1,col=4,drawlabels=FALSE,add=TRUE)
+  }
+}
+
